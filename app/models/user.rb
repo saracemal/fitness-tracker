2fbf999ab9966e84b4b9bae2204e46cb2432839a
februar-y <jarmah241@gmail.com>
2020-12-02 18:49:46 -0500

    waste changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
     
     has_secure_password 
     # validates :name, presence: true { "must be created with a name"}
-    validates :user_name, uniqueness: true {"Username is already in use"}
+    #validates :user_name, uniqueness: true {"Username is already in use"}
 
     #or
     validates_presence_of :user_name, :password, :name, :age, :weight, :height 
