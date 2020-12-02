ae6d83b87ce6546bb2a8301eb03f1eb9af710347
Sara Cemal <you@example.com>
2020-12-01 21:44:27 -0500

    validations

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -2,7 +2,7 @@ class Exercise < ApplicationRecord
     has_many :routine_exercises
     has_many :routines, through: :routine_exercises
 
-    # validates_presence_of :name, :difficulty
+     validates_presence_of :name, :difficulty
     # # validates :difficulty, inclusion: { in: %w(beginner intermediate advanced),
     # # message: "%{value} is not a valid difficulty" }
 end
