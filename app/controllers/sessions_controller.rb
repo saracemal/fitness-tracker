1d0c463ca31615204f9a89ff69e045c3a6234a8b
februar-y <jarmah241@gmail.com>
2020-12-04 01:46:46 -0500

    added bootstrap, started testing

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,7 +17,7 @@ class SessionsController < ApplicationController
             redirect_to user_path(user)
         else
             #flash[:errors] = user.errors.full_messages
-            redirect_to new_user_path
+            redirect_to new_login_path
         end
     end
 
