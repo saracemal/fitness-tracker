553bc888dbe9c2c5c3a6f6446ea0c2eb6c888c7a
februar-y <jarmah241@gmail.com>
2020-12-07 10:36:18 -0500

    imgs added to user index page and show page draft

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,16 @@ class UsersController < ApplicationController
 
     def new
         @user = User.new
+        @img_url = [
+            "https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX4617170.jpg",
+            "https://i.pinimg.com/originals/4f/bb/48/4fbb4825cba6b7ce8abf86e9722c87cb.jpg",
+            "https://i.pinimg.com/564x/71/56/14/715614be795e5d80f759c7d9e9d4b993.jpg",
+            "https://i.pinimg.com/474x/a9/e9/64/a9e964af54fa741354cabd86e280d768.jpg",
+            "https://i.pinimg.com/564x/36/9b/c0/369bc01e4c301bff41711a2241a4213f.jpg",
+            "https://www.poynter.org/wp-content/uploads/2019/12/AP_19345327608569.jpg",
+            "https://c8.alamy.com/comp/HHB2JH/funny-crazy-hairdresser-haircut-hair-man-humans-human-beings-people-HHB2JH.jpg",
+            "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/idris-elba-says-coronavirus-had-a-22traumatic-22-impact-on-his-mental-health-1594723325.jpg"
+        ]
     end
 
     def create
@@ -72,9 +82,12 @@ class UsersController < ApplicationController
         @routine = Routine.find(params[:id])
     end
 
+    
+
     private
     def user_params
-        params.require(:user).permit(:name, :age, :weight, :height, :password, :user_name)
+        params.require(:user).permit(:name, :age, :weight, :height, :password, :user_name, :img_url)
     end
+
 
 end
